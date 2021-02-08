038eebee0c0ad5e9404e862e05312839d244777c
dornescum <dornescu@gmail.com>
2021-02-08 17:31:00 +0200

    modal cu slot si $emit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!--  <img alt="Vue logo" src="./assets/logo.png">-->
+  <h1>{{ title }}</h1>
+  <p> Welcome ...</p>
+  <div v-if="showModal">
+    <!--        @close ("close") pt ca am folosit $emit-->
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+      <a href="">test </a>
+      <a href="">test1</a>
+      </template>
+      <h1>some shit</h1>
+      <p>other shit</p>
+    </Modal>
+    >
+  </div>
+  <button @click.once="toggleModal">open modal</button>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      title: 'My First Vue App!',
+      showModal: false,
+      // showModalTwo: false,
+      text: "some shit",
+      header: "other shit"
+    }
+  },
   components: {
-    HelloWorld
+    Modal
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
   }
 }
 </script>
